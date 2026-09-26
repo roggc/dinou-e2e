206e14fd83ed4177f717d4694a75c23a5c219b38
roggc <roggc9@gmail.com>
2026-09-26 19:52:28 +0200

    timeout in test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -256,13 +256,13 @@ test.describe("🏗️ Tests de Generación Estática Completa", () => {
     // await expect(pageA.getByText("Hello ALICE")).toBeVisible();
     await expect(
       pageA.getByText("Hello ALICE", { exact: true }).locator("visible=true"),
-    ).toBeVisible({ timeout: 15000 });
+    ).toBeVisible({ timeout: 30000 });
     await expect(pageA.getByText("Hello BOB")).not.toBeVisible(); // 🛑 Si esto falla, tienes un leak grave
 
     // await expect(pageB.getByText("Hello BOB")).toBeVisible();
     await expect(
       pageB.getByText("Hello BOB", { exact: true }).locator("visible=true"),
-    ).toBeVisible({ timeout: 15000 });
+    ).toBeVisible({ timeout: 30000 });
     await expect(pageB.getByText("Hello ALICE")).not.toBeVisible();
 
     await userA.close();
